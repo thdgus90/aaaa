d6331e4e2fd0de9f8cd17d1f129773928db0d85d
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-10-09 23:29:32 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,84 +1,89 @@
 /* eslint-disable */
 
-import React, {useState} from 'react';
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-import './App.css';
-import Data from './data.js';
-import Detail from './Detail.js';
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import "./App.css";
+import Data from "./data.js";
+import Detail from "./Detail.js";
 
-import {Link, Route, Switch} from 'react-router-dom'
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
 
-    let [shoes, shoes변경] = useState(Data);
-
-    return (
-        <div className="App">
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                            <Nav.Link><Link to="/Detail">Detail</Link></Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider/>
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Switch>
-            <Route exact="exact" path="/">
-            <div className="one">
-                <h1>20% Season off</h1>
-                <div>우리집 상품이 세일중</div>
-                <button className="button">바로가기</button>
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="Detail">
+                Detail
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Switch>
+        <Route exact="exact" path="/">
+          <div className="one">
+            <h1>20% Season off</h1>
+            <div>우리집 상품이 세일중</div>
+            <button className="button">바로가기</button>
+          </div>
+          <div className="container">
+            <div className="row">
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i} key={i} />;
+              })}
             </div>
-            <div className="container">
-                <div className="row">
-                    {
-                        shoes.map((a, i) => {
-                            return <Card shoes={shoes[i]} i={i} key={i}/>
-                        })
-                    }
-                </div>
-            </div>
-            </Route>
-
-                        /detail/:id (아무문자나 받겠다는 url작명법)  
-            <Route path="/detail/:id">
-                <Detail shoes={shoes}/>
-            </Route>
-
-
-            <Route path="/:id">
-                    <div>아무거나 적엇을때 이거보여주셈</div>
-            </Route>
-                    </Switch>
-        </div>
-
-    );
+          </div>
+        </Route>
+        /detail/:id (아무문자나 받겠다는 url작명법)
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
+        </Route>
+        <Route path="/:id">
+          <div>아무거나 적엇을때 이거보여주셈</div>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 function Card(props) {
-    return (
-        <div className="col-md-4">
-            <img
-                src={'https://codingapple1.github.io/shop/shoes' + (
-                    props.i + 1
-                ) + '.jpg'}
-                width="100%"/>
-            <h4>{props.shoes.title}</h4>
-            <p>{props.shoes.content}& {props.shoes.price}</p>
-        </div>
-
-    )
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content}& {props.shoes.price}
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/Detail.js
+++ src/Detail.js
@@ -1,47 +1,93 @@
 /* eslint-disable */
 
-import React from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.css";
 
 let 박스 = styled.div`
-    padding : 20px;
+  padding: 20px;
 `;
 
 //$ 변수명 넣는문법
 let 제목 = styled.h4`
-    font-size: 25px;
-    color: ${ props => props.색상 }
+  font-size: 25px;
+  color: ${(props) => props.색상};
 `;
 
-function Detail(props){
-    
-    let { id } = useParams();
-    //histoty는 방문기록을 저장해놓는 object
-    let 찾은상품 = props.shoes.find(function(상품){
-        return 상품.id == id
-    })
-    let history = useHistory();
+function Detail(props) {
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState("");
+
+  useEffect(() => {
+    let 타이머 = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    console.log("dd");
+    return () => {
+      clearTimeout(타이머);
+    };
+  }, [alert]);
+  // []가 빈칸일시 처음딱 한번만 실행하고 실행하지 않음
+  //   alert라는 state가 변경 될때만 실행
+  //금 Detail 방문시 2초 후에 UI 사라지게 해주세요~ 라고 코드를 짰습니다.
+
+  // 근데 2초가 되기도 전에 Detail을 벗어나면 어떻게 될까요?
+
+  // 지금은 별 문제없는 것 같지만 코드가 길어지거나 꼬이면
+
+  // 남아있는 타이머 때문에 이상한 현상이 일어날 수 있습니다.
+
+  // 그래서 컴포넌트가 사라질 때 타이머를 없애는 코드도 추가해주는게 좋습니다.
+  // useEffect안에는 return + 함수를 추가하면
+
+  // 컴포넌트가 사라질 때 특정 코드를 실행할 수 있다고 했습니다.
+
+  // 거기에 clearTimeout을 추가한겁니다.
+
+  let { id } = useParams();
+  //histoty는 방문기록을 저장해놓는 object
+  let 찾은상품 = props.shoes.find(function (상품) {
+    return 상품.id == id;
+  });
+  let history = useHistory();
 
   return (
     <div className="container">
-        <박스>
-            <제목 색상="red">Detail</제목>
-        </박스>
+      <박스>
+        <제목 className="red">Detail</제목>
+      </박스>
+      {inputData}
+      {/* //input에 값이 변경 될때마다 inputData변경으로 바꿔줌// */}
+      <input
+        onChange={(e) => {
+          inputData변경(e.target.value);
+        }}
+      />
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
         </div>
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
-          <button className="btn btn-danger">주문하기</button> 
-          <button className="btn btn-danger" onClick={() => { history.push('/')}}>뒤로가기</button> 
+          <button className="btn btn-danger">주문하기</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            뒤로가기
+          </button>
         </div>
       </div>
-  </div>  
-  )
-};
+    </div>
+  );
+}
 
-export default Detail ;
+export default Detail;
