dd7c67f9493fe50f02ac97d251813ebb85a959ec
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-10-09 16:43:07 +0900

    find 함수

--- src/Detail.js
+++ src/Detail.js
@@ -7,6 +7,9 @@ function Detail(props){
     
     let { id } = useParams();
     //histoty는 방문기록을 저장해놓는 object
+    let 찾은상품 = props.shoes.find(function(상품){
+        return 상품.id == id
+    })
     let history = useHistory();
 
   return (
@@ -16,9 +19,9 @@ function Detail(props){
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].title}</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
           <button className="btn btn-danger" onClick={() => { history.push('/')}}>뒤로가기</button> 
         </div>
